1ab0df47457ac5f0b2c9dfbc3afc7072d316b9e5
letouque <letouque@gmail.com>
2025-11-24 19:30:06 +0100

    Update songs_L.js

--- songs/songs_L.js
+++ songs/songs_L.js
@@ -3,19 +3,23 @@ const songsL = [
     "artist": "Laura Branigan",
     "title": "Self Control",
     "duration": "3 min 44",
-    "style": [
-      "Pop"
-    ],
+    "style": ["Pop"],
     "category": "80s Music",
     "cover": "img/Laura Branigan - Self Control.jpg"
+  },
+  {
+    "artist": "Linkin Park",
+    "title": "One Step Closer",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Linkin Park - One Step Closer.jpg"
   },
   {
     "artist": "Liquido",
     "title": "Narcotic",
     "duration": "4 min 23",
-    "style": [
-      "Alternative Rock"
-    ],
+    "style": ["Alternative Rock"],
     "category": "90s Music",
     "cover": "img/Liquido - Narcotic.jpg"
   },
@@ -23,9 +27,7 @@ const songsL = [
     "artist": "LocoRoco",
     "title": "Menu",
     "duration": "1 min 10",
-    "style": [
-      "Video Game Music"
-    ],
+    "style": ["Video Game Music"],
     "category": "2000s Music",
     "cover": "img/LocoRoco - Menu.jpg"
   },
@@ -33,9 +35,7 @@ const songsL = [
     "artist": "Lykke Li",
     "title": "I Follow Rivers (Tiesto Remix)",
     "duration": "3 min 16",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2010s Music",
     "cover": "img/Lykke Li - I Follow Rivers (Tiesto Remix).jpg"
   }
