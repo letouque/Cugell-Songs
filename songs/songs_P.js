fe91465ec931865b8123d8ed8264eb214213976c
letouque <letouque@gmail.com>
2025-11-24 19:47:31 +0100

    Update songs_P.js

--- songs/songs_P.js
+++ songs/songs_P.js
@@ -1,5 +1,14 @@
 const songsP = [
   {
+    "artist": "Panjabi MC",
+    "title": "Mundian To Bach Ke",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Panjabi MC - Mundian To Bach Ke.jpg"
+  },
+
+  {
     "artist": "Paolo Conte",
     "title": "Via con me",
     "duration": "",
@@ -7,56 +16,43 @@ const songsP = [
     "category": "",
     "cover": "img/Paolo Conte - Via con me.jpg"
   },
+
   {
     "artist": "Paul Kalkbrenner & Fritz Kalkbrenner",
     "title": "Sky and sand",
     "duration": "3 min 54",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2000s Music",
     "cover": "img/Paul Kalkbrenner & Fritz Kalkbrenner - Sky and sand.jpg"
   },
+
   {
     "artist": "Peter Gabriel",
     "title": "Solsbury Hill",
     "duration": "4 min 57",
-    "style": [
-      "Rock"
-    ],
+    "style": ["Rock"],
     "category": "70s Music",
     "cover": "img/Peter Gabriel - Solsbury Hill.jpg"
   },
+
   {
     "artist": "Phil Collins",
     "title": "In the air tonight",
     "duration": "4 min 29",
-    "style": [
-      "Rock"
-    ],
+    "style": ["Rock"],
     "category": "80s Music",
     "cover": "img/Phil Collins - In the air tonight.jpg"
   },
+
   {
     "artist": "Pixies",
     "title": "Where is my mind",
     "duration": "3 min 45",
-    "style": [
-      "Alternative Rock"
-    ],
+    "style": ["Alternative Rock"],
     "category": "80s Music",
     "cover": "img/Pixies - Where is my mind.jpg"
   },
-  {
-    "artist": "Placebo",
-    "title": "Running up that hill",
-    "duration": "4 min 52",
-    "style": [
-      "Alternative Rock"
-    ],
-    "category": "2000s Music",
-    "cover": "img/Placebo - Running up that hill.jpg"
-  },
+
   {
     "artist": "Placebo",
     "title": "Pure Morning",
@@ -66,22 +62,44 @@ const songsP = [
     "cover": "img/Placebo - Pure Morning.jpg"
   },
   {
+    "artist": "Placebo",
+    "title": "Running up that hill",
+    "duration": "4 min 52",
+    "style": ["Alternative Rock"],
+    "category": "2000s Music",
+    "cover": "img/Placebo - Running up that hill.jpg"
+  },
+  {
+    "artist": "Placebo",
+    "title": "Special K",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Placebo - Special K.jpg"
+  },
+  {
+    "artist": "Placebo",
+    "title": "Special Needs",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Placebo - Special Needs.jpg"
+  },
+
+  {
     "artist": "Police",
     "title": "Every breath you take",
     "duration": "3 min 57",
-    "style": [
-      "Rock"
-    ],
+    "style": ["Rock"],
     "category": "80s Music",
     "cover": "img/Police - Every breath you take.jpg"
   },
+
   {
     "artist": "Prince",
     "title": "Purple rain",
     "duration": "6 min 26",
-    "style": [
-      "Pop Rock"
-    ],
+    "style": ["Pop Rock"],
     "category": "80s Music",
     "cover": "img/Prince - Purple rain.jpg"
   }
