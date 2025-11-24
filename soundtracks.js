c33932dbc3a51a5d8ff41f7c65e8019785c33cf5
letouque <letouque@gmail.com>
2025-11-24 16:14:38 +0100

    Create soundtracks.js

---
+++ soundtracks.js
@@ -0,0 +1,21 @@
+const soundtracks = [
+  {
+    artist: "Nobuo Uematsu",
+    title: "To Zanarkand",
+    duration: "3:12",
+    year: "2001",
+    style: ["Game OST","Piano"],
+    category: "2000s Music",
+    cover: "img/Nobuo Uematsu - To Zanarkand.jpg"
+  },
+  {
+    artist: "Yoko Shimomura",
+    title: "Dearly Beloved",
+    duration: "2:43",
+    year: "2002",
+    style: ["Game OST","Piano"],
+    category: "2000s Music",
+    cover: "img/Yoko Shimomura - Dearly Beloved.jpg"
+  },
+  // etc.
+];
