7580cfe67d08aceeb181d89bc5b7862e64758c83
letouque <letouque@gmail.com>
2025-11-24 21:19:23 +0100

    Update songs_V.js

--- songs/songs_V.js
+++ songs/songs_V.js
@@ -1,11 +1,18 @@
 const songsV = [
   {
+    "artist": "Van Halen",
+    "title": "Jump",
+    "duration": "3 min 43",
+    "style": ["Hard Rock"],
+    "category": "80s Music",
+    "cover": "img/Van Halen - Jump.jpg"
+  },
+
+  {
     "artist": "Vampire Weekend",
     "title": "A-Punk",
     "duration": "2 min 13",
-    "style": [
-      "Indie Rock"
-    ],
+    "style": ["Indie Rock"],
     "category": "2000s Music",
     "cover": "img/Vampire Weekend - A-Punk.jpg"
   },
@@ -13,20 +20,17 @@ const songsV = [
     "artist": "Vampire Weekend",
     "title": "White Sky",
     "duration": "2 min 57",
-    "style": [
-      "Indie Rock"
-    ],
+    "style": ["Indie Rock"],
     "category": "2010s Music",
     "cover": "img/Vampire Weekend - White Sky.jpg"
   },
+
   {
-    "artist": "Van Halen",
-    "title": "Jump",
-    "duration": "3 min 43",
-    "style": [
-      "Hard Rock"
-    ],
-    "category": "80s Music",
-    "cover": "img/Van Halen - Jump.jpg"
+    "artist": "Visage",
+    "title": "Fade to grey",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Visage - Fade to grey.jpg"
   }
 ];
