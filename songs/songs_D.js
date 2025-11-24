10718cdce8db6a08bee86b8f41297b19f6dec3ab
letouque <letouque@gmail.com>
2025-11-24 19:14:11 +0100

    Update songs_D.js

--- songs/songs_D.js
+++ songs/songs_D.js
@@ -3,39 +3,56 @@ const songsD = [
     "artist": "Da Hool",
     "title": "Meet Her At The Love Parade",
     "duration": "2 min 44",
-    "style": [
-      "Dance"
-    ],
+    "style": ["Dance"],
     "category": "90s Music",
     "cover": "img/Da Hool - Meet Her At The Love Parade.jpg"
   },
+
   {
     "artist": "Daft Punk",
     "title": "Aerodynamic",
     "duration": "3 min 35",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2000s Music",
     "cover": "img/Daft Punk - Aerodynamic.jpg"
   },
   {
     "artist": "Daft Punk",
+    "title": "Around the world",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Daft Punk - Around the world.jpg"
+  },
+  {
+    "artist": "Daft Punk",
+    "title": "Contact",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Daft Punk - Contact.jpg"
+  },
+  {
+    "artist": "Daft Punk",
     "title": "Digital Love",
     "duration": "4 min 46",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2000s Music",
     "cover": "img/Daft Punk - Digital Love.jpg"
   },
   {
     "artist": "Daft Punk",
+    "title": "Face to Face",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Daft Punk - Face to Face.jpg"
+  },
+  {
+    "artist": "Daft Punk",
     "title": "Giorgio",
     "duration": "5 min 51",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2010s Music",
     "cover": "img/Daft Punk - Giorgio.jpg"
   },
@@ -43,19 +60,23 @@ const songsD = [
     "artist": "Daft Punk",
     "title": "Harder better faster stronger",
     "duration": "3 min 38",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2000s Music",
     "cover": "img/Daft Punk - Harder better faster stronger.jpg"
   },
   {
     "artist": "Daft Punk",
+    "title": "Human after all",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Daft Punk - Human after all.jpg"
+  },
+  {
+    "artist": "Daft Punk",
     "title": "Instant Crush",
     "duration": "5 min 14",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2010s Music",
     "cover": "img/Daft Punk - Instant Crush.jpg"
   },
@@ -63,9 +84,7 @@ const songsD = [
     "artist": "Daft Punk",
     "title": "Lose yourself to dance",
     "duration": "5 min 54",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2010s Music",
     "cover": "img/Daft Punk - Lose yourself to dance.jpg"
   },
@@ -73,9 +92,7 @@ const songsD = [
     "artist": "Daft Punk",
     "title": "One More Time",
     "duration": "3 min 52",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2000s Music",
     "cover": "img/Daft Punk - One More Time.jpg"
   },
@@ -83,9 +100,7 @@ const songsD = [
     "artist": "Daft Punk",
     "title": "Robot Rock",
     "duration": "4 min 45",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2000s Music",
     "cover": "img/Daft Punk - Robot Rock.jpg"
   },
@@ -93,9 +108,7 @@ const songsD = [
     "artist": "Daft Punk",
     "title": "Television Rules the Nation",
     "duration": "4 min 05",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2000s Music",
     "cover": "img/Daft Punk - Television Rules the Nation.jpg"
   },
@@ -103,9 +116,7 @@ const songsD = [
     "artist": "Daft Punk",
     "title": "Veridis Quo",
     "duration": "2 min 35",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2000s Music",
     "cover": "img/Daft Punk - Veridis Quo.jpg"
   },
@@ -113,109 +124,105 @@ const songsD = [
     "artist": "Daft Punk",
     "title": "Within",
     "duration": "3 min 33",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2010s Music",
     "cover": "img/Daft Punk - Within.jpg"
   },
+
   {
     "artist": "Daniel Balavoine",
     "title": "Tout les cris les SOS",
     "duration": "4 min 42",
-    "style": [
-      "French Pop"
-    ],
+    "style": ["French Pop"],
     "category": "80s Music",
     "cover": "img/Daniel Balavoine - Tout les cris les SOS.jpg"
   },
+
   {
     "artist": "Dario G",
     "title": "Sunchyme",
     "duration": "3 min 57",
-    "style": [
-      "Dance"
-    ],
+    "style": ["Dance"],
     "category": "90s Music",
     "cover": "img/Dario G - Sunchyme.jpg"
   },
+
   {
     "artist": "David Bowie",
     "title": "Life on mars",
     "duration": "3 min 22",
-    "style": [
-      "Glam Rock"
-    ],
+    "style": ["Glam Rock"],
     "category": "70s Music",
     "cover": "img/David Bowie - Life on mars.jpg"
   },
+
   {
     "artist": "Desireless",
     "title": "Voyage Voyage",
     "duration": "4 min 17",
-    "style": [
-      "French Pop"
-    ],
+    "style": ["French Pop"],
     "category": "80s Music",
     "cover": "img/Desireless - Voyage Voyage.jpg"
   },
+
   {
     "artist": "Dexys Midnight Runners",
     "title": "Come on Eileen",
     "duration": "4 min 19",
-    "style": [
-      "Pop Rock"
-    ],
+    "style": ["Pop Rock"],
     "category": "80s Music",
     "cover": "img/Dexys Midnight Runners - Come on Eileen.jpg"
   },
+
   {
     "artist": "Dire Straits",
     "title": "Brothers in arms",
     "duration": "6 min 30",
-    "style": [
-      "Rock"
-    ],
+    "style": ["Rock"],
     "category": "80s Music",
     "cover": "img/Dire Straits - Brothers in arms.jpg"
   },
   {
+    "artist": "Dire Straits",
+    "title": "Sultans of Swing",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Dire Straits - Sultans of Swing.jpg"
+  },
+
+  {
     "artist": "DJ Okawari",
     "title": "Flower Dance",
     "duration": "4 min 22",
-    "style": [
-      "Instrumental"
-    ],
+    "style": ["Instrumental"],
     "category": "2010s Music",
     "cover": "img/DJ Okawari - Flower Dance.jpg"
   },
+
   {
     "artist": "Donna Summer",
     "title": "Hot Stuff",
     "duration": "3 min 46",
-    "style": [
-      "Disco"
-    ],
+    "style": ["Disco"],
     "category": "70s Music",
     "cover": "img/Donna Summer - Hot Stuff.jpg"
   },
+
   {
     "artist": "Dune (1984)",
     "title": "Take My Hand (Credits music)",
     "duration": "4 min 17",
-    "style": [
-      "Soundtrack"
-    ],
+    "style": ["Soundtrack"],
     "category": "80s Music",
     "cover": "img/Dune (1984) - Take My Hand (Credits music).jpg"
   },
+
   {
     "artist": "Duran Duran",
     "title": "Ordinary World",
     "duration": "4 min 59",
-    "style": [
-      "Pop Rock"
-    ],
+    "style": ["Pop Rock"],
     "category": "90s Music",
     "cover": "img/Duran Duran - Ordinary World.jpg"
   }
