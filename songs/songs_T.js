7cf90f2a09ae8de591b4e83836775d50ea7119a0
letouque <letouque@gmail.com>
2025-11-24 21:17:14 +0100

    Update songs_T.js

--- songs/songs_T.js
+++ songs/songs_T.js
@@ -3,9 +3,7 @@ const songsT = [
     "artist": "Talk Talk",
     "title": "It's my life",
     "duration": "4 min 21",
-    "style": [
-      "New Wave"
-    ],
+    "style": ["New Wave"],
     "category": "80s Music",
     "cover": "img/Talk Talk - It's my life.jpg"
   },
@@ -13,19 +11,16 @@ const songsT = [
     "artist": "Talk Talk",
     "title": "Such a shame",
     "duration": "4 min 17",
-    "style": [
-      "New Wave"
-    ],
+    "style": ["New Wave"],
     "category": "80s Music",
     "cover": "img/Talk Talk - Such a shame.jpg"
   },
+
   {
     "artist": "Tame Impala",
     "title": "Borderline",
     "duration": "4 min 34",
-    "style": [
-      "Psychedelic Pop"
-    ],
+    "style": ["Psychedelic Pop"],
     "category": "2010s Music",
     "cover": "img/Tame Impala - Borderline.jpg"
   },
@@ -33,9 +28,7 @@ const songsT = [
     "artist": "Tame Impala",
     "title": "Elephant",
     "duration": "3 min 37",
-    "style": [
-      "Psychedelic Rock"
-    ],
+    "style": ["Psychedelic Rock"],
     "category": "2010s Music",
     "cover": "img/Tame Impala - Elephant.jpg"
   },
@@ -43,49 +36,69 @@ const songsT = [
     "artist": "Tame Impala",
     "title": "The Less I know the better",
     "duration": "3 min 29",
-    "style": [
-      "Psychedelic Pop"
-    ],
+    "style": ["Psychedelic Pop"],
     "category": "2010s Music",
     "cover": "img/Tame Impala - The Less I know the better.jpg"
   },
+
   {
     "artist": "Tanita Tikaram",
     "title": "Twist in My Sobriety",
     "duration": "3 min 50",
-    "style": [
-      "Pop"
-    ],
+    "style": ["Pop"],
     "category": "80s Music",
     "cover": "img/Tanita Tikaram - Twist in My Sobriety.jpg"
   },
+
+  {
+    "artist": "The Beatles",
+    "title": "While My Guitar Gently Weeps",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/The Beatles - While My Guitar Gently Weeps.jpg"
+  },
+
   {
     "artist": "The Buggles",
     "title": "Video killed the radio star",
     "duration": "4 min 14",
-    "style": [
-      "Synthpop"
-    ],
+    "style": ["Synthpop"],
     "category": "70s Music",
     "cover": "img/The Buggles - Video killed the radio star.jpg"
   },
+
+  {
+    "artist": "The Corrs",
+    "title": "Runaway",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/The Corrs - Runaway.jpg"
+  },
+  {
+    "artist": "The Corrs",
+    "title": "Toss the Feathers",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/The Corrs - Toss the Feathers.jpg"
+  },
+
   {
     "artist": "The Cranberries",
     "title": "Zombie",
     "duration": "5 min 03",
-    "style": [
-      "Alternative Rock"
-    ],
+    "style": ["Alternative Rock"],
     "category": "90s Music",
     "cover": "img/The Cranberries - Zombie.jpg"
   },
+
   {
     "artist": "The Cure",
     "title": "Close to me",
     "duration": "3 min 29",
-    "style": [
-      "New Wave"
-    ],
+    "style": ["New Wave"],
     "category": "80s Music",
     "cover": "img/The Cure - Close to me.jpg"
   },
@@ -93,52 +106,65 @@ const songsT = [
     "artist": "The Cure",
     "title": "Lullaby",
     "duration": "4 min 09",
-    "style": [
-      "New Wave"
-    ],
+    "style": ["New Wave"],
     "category": "80s Music",
     "cover": "img/The Cure - Lullaby.jpg"
   },
+
   {
     "artist": "The Doors",
     "title": "Riders on the storm",
     "duration": "2 min 46",
-    "style": [
-      "Psychedelic Rock"
-    ],
+    "style": ["Psychedelic Rock"],
     "category": "70s Music",
     "cover": "img/The Doors - Riders on the storm.jpg"
   },
+
   {
     "artist": "The Pointer Sisters",
     "title": "Im so excited",
     "duration": "5 min 33",
-    "style": [
-      "Pop"
-    ],
+    "style": ["Pop"],
     "category": "80s Music",
     "cover": "img/The Pointer Sisters - Im so excited.jpg"
   },
+
+  {
+    "artist": "The Script feat Will-i-am",
+    "title": "Hall of fame",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/The Script feat Will-i-am - Hall of fame.jpg"
+  },
+
+  {
+    "artist": "The Stranglers",
+    "title": "Golden Brown",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/The Stranglers - Golden Brown.jpg"
+  },
+
   {
     "artist": "The Weeknd",
     "title": "Save your tears",
     "duration": "3 min 33",
-    "style": [
-      "Pop"
-    ],
+    "style": ["Pop"],
     "category": "2020s Music",
     "cover": "img/The Weeknd - Save your tears.jpg"
   },
+
   {
     "artist": "The XX",
     "title": "Intro",
     "duration": "2 min 00",
-    "style": [
-      "Indie Pop"
-    ],
+    "style": ["Indie Pop"],
     "category": "2000s Music",
     "cover": "img/The XX - Intro.jpg"
   },
+
   {
     "artist": "Timbaland",
     "title": "The way I are",
@@ -147,23 +173,21 @@ const songsT = [
     "category": "",
     "cover": "img/Timbaland - The way I are.jpg"
   },
+
   {
     "artist": "Tomoyasu Hotei",
     "title": "Battle Without Honor or Humanity",
     "duration": "1 min 53",
-    "style": [
-      "Soundtrack"
-    ],
+    "style": ["Soundtrack"],
     "category": "2000s Music",
     "cover": "img/Tomoyasu Hotei - Battle Without Honor or Humanity.jpg"
   },
+
   {
     "artist": "Toto",
     "title": "Africa",
     "duration": "4 min 12",
-    "style": [
-      "Rock"
-    ],
+    "style": ["Rock"],
     "category": "80s Music",
     "cover": "img/Toto - Africa.jpg"
   },
@@ -171,29 +195,25 @@ const songsT = [
     "artist": "Toto",
     "title": "Rosana",
     "duration": "5 min 16",
-    "style": [
-      "Rock"
-    ],
+    "style": ["Rock"],
     "category": "80s Music",
     "cover": "img/Toto - Rosana.jpg"
   },
+
   {
     "artist": "Tracy Chapman",
     "title": "Talkin' about a revolution",
     "duration": "2 min 40",
-    "style": [
-      "Folk Rock"
-    ],
+    "style": ["Folk Rock"],
     "category": "80s Music",
     "cover": "img/Tracy Chapman - Talkin' about a revolution.jpg"
   },
+
   {
     "artist": "Twenty One Pilots",
     "title": "Stressed out",
     "duration": "3 min 18",
-    "style": [
-      "Alternative"
-    ],
+    "style": ["Alternative"],
     "category": "2010s Music",
     "cover": "img/Twenty One Pilots - Stressed out.jpg"
   }
