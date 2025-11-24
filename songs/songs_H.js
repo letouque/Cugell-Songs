5d33f9f338375ccfe7c6594723648643c0e275f2
letouque <letouque@gmail.com>
2025-11-24 19:24:10 +0100

    Update songs_H.js

--- songs/songs_H.js
+++ songs/songs_H.js
@@ -1,11 +1,17 @@
 const songsH = [
   {
+    "artist": "Hall and Oates",
+    "title": "Out of touch",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Hall and Oates - Out of touch.jpg"
+  },
+  {
     "artist": "Highway Men",
     "title": "Highwayman",
     "duration": "3 min 00",
-    "style": [
-      "Country"
-    ],
+    "style": ["Country"],
     "category": "80s Music",
     "cover": "img/Highway Men - Highwayman.jpg"
   }
