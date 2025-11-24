07264de155aa333af908311647ca7b2b984338f4
letouque <letouque@gmail.com>
2025-11-24 17:23:49 +0100

    Create zelda.js

---
+++ soundtracks/zelda.js
@@ -0,0 +1,28 @@
+const zelda = [
+  {
+    artist: "",
+    title: "Hyrule",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Game",
+    gameUniverse: "Nintendo",
+    finalFantasy: "",
+    license: "Zelda",
+    style: ["Game OST"],
+    cover: "img/Zelda - Hyrule.jpg"
+  },
+  {
+    artist: "",
+    title: "Light World",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Game",
+    gameUniverse: "Nintendo",
+    finalFantasy: "",
+    license: "Zelda",
+    style: ["Game OST"],
+    cover: "img/Zelda - Light World.jpg"
+  }
+];
