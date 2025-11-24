ad6f8a251d7a1c65d876c8080f56ecd61fe1a844
letouque <letouque@gmail.com>
2025-11-24 19:29:09 +0100

    Update songs_K.js

--- songs/songs_K.js
+++ songs/songs_K.js
@@ -1,24 +1,31 @@
 const songsK = [
   {
+    "artist": "Kate Bush",
+    "title": "Wuthering heights",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Kate Bush - Wuthering heights.jpg"
+  },
+
+  {
     "artist": "Keane",
     "title": "Somewhere Only We Know",
     "duration": "3 min 52",
-    "style": [
-      "Pop Rock"
-    ],
+    "style": ["Pop Rock"],
     "category": "2000s Music",
     "cover": "img/Keane - Somewhere Only We Know.jpg"
   },
+
   {
     "artist": "Kid Francescoli",
     "title": "Moon",
     "duration": "6 min 18",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2010s Music",
     "cover": "img/Kid Francescoli - Moon.jpg"
   },
+
   {
     "artist": "Killers",
     "title": "Human",
@@ -27,26 +34,33 @@ const songsK = [
     "category": "",
     "cover": "img/Killers - Human.jpg"
   },
+
   {
     "artist": "Kim Carnes",
     "title": "Bette Davis Eyes",
     "duration": "3 min 49",
-    "style": [
-      "Pop Rock"
-    ],
+    "style": ["Pop Rock"],
     "category": "80s Music",
     "cover": "img/Kim Carnes - Bette Davis Eyes.jpg"
   },
+
   {
     "artist": "Kool & the Gang",
     "title": "Fresh",
     "duration": "3 min 22",
-    "style": [
-      "Funk"
-    ],
+    "style": ["Funk"],
     "category": "80s Music",
     "cover": "img/Kool & the Gang - Fresh.jpg"
   },
+  {
+    "artist": "Kool and the Gang",
+    "title": "Jungle Boogie",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Kool and the Gang - Jungle Boogie.jpg"
+  },
+
   {
     "artist": "Kraftwerk",
     "title": "The Model",
