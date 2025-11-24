a73cd675c4c4d2cb8f581f45da8bf801d30feeb5
letouque <letouque@gmail.com>
2025-11-24 21:09:19 +0100

    Update songs_R.js

--- songs/songs_R.js
+++ songs/songs_R.js
@@ -3,19 +3,32 @@ const songsR = [
     "artist": "R3HAB",
     "title": "All Around The World (La La La)",
     "duration": "2 min 29",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2010s Music",
     "cover": "img/R3HAB - All Around The World (La La La).jpg"
+  },
+
+  {
+    "artist": "Radiohead",
+    "title": "Creep",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Radiohead - Creep.jpg"
+  },
+  {
+    "artist": "Radiohead",
+    "title": "Jigsaw Falling into Place",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Radiohead - Jigsaw Falling into Place.jpg"
   },
   {
     "artist": "Radiohead",
     "title": "No Surprises",
     "duration": "3 min 56",
-    "style": [
-      "Alternative Rock"
-    ],
+    "style": ["Alternative Rock"],
     "category": "90s Music",
     "cover": "img/Radiohead - No Surprises.jpg"
   },
@@ -23,42 +36,55 @@ const songsR = [
     "artist": "Radiohead",
     "title": "Street Spirit (Faded out)",
     "duration": "4 min 07",
-    "style": [
-      "Alternative Rock"
-    ],
+    "style": ["Alternative Rock"],
     "category": "90s Music",
     "cover": "img/Radiohead - Street Spirit (Faded out).jpg"
   },
+
   {
     "artist": "Ram Jam",
     "title": "Black Betty",
     "duration": "3 min 57",
-    "style": [
-      "Hard Rock"
-    ],
+    "style": ["Hard Rock"],
     "category": "70s Music",
     "cover": "img/Ram Jam - Black Betty.jpg"
   },
+
   {
     "artist": "Rammstein",
     "title": "Adieu",
     "duration": "4 min 30",
-    "style": [
-      "Industrial Metal"
-    ],
+    "style": ["Industrial Metal"],
     "category": "2020s Music",
     "cover": "img/Rammstein - Adieu.jpg"
   },
+
   {
     "artist": "Ramones",
     "title": "Blitzkrieg Bop",
     "duration": "2 min 13",
-    "style": [
-      "Punk Rock"
-    ],
+    "style": ["Punk Rock"],
     "category": "70s Music",
     "cover": "img/Ramones - Blitzkrieg Bop.jpg"
   },
+
+  {
+    "artist": "REM",
+    "title": "Losing my Religion",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/REM - Losing my Religion.jpg"
+  },
+  {
+    "artist": "REM",
+    "title": "Shiny Happy People",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/REM - Shiny Happy People.jpg"
+  },
+
   {
     "artist": "Red Hot Chili Peppers",
     "title": "Can't Stop",
@@ -75,63 +101,92 @@ const songsR = [
     "category": "",
     "cover": "img/Red Hot Chili Peppers - Road Trippin'.jpg"
   },
+
   {
     "artist": "Ricchi e Poveri",
     "title": "Sara perche ti amo",
     "duration": "3 min 13",
-    "style": [
-      "Pop"
-    ],
+    "style": ["Pop"],
     "category": "80s Music",
     "cover": "img/Ricchi e Poveri - Sara perche ti amo.jpg"
   },
+
   {
     "artist": "Robert Miles",
     "title": "Children",
     "duration": "4 min 03",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "90s Music",
     "cover": "img/Robert Miles - Children.jpg"
   },
+
+  {
+    "artist": "Rod Stewart",
+    "title": "Baby Jane",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Rod Stewart - Baby Jane.jpg"
+  },
+  {
+    "artist": "Rod Stewart",
+    "title": "Do you think I'm sexy",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Rod Stewart - Do you think I'm sexy.jpg"
+  },
+
+  {
+    "artist": "Roger Glover",
+    "title": "Love is all",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Roger Glover - Love is all.jpg"
+  },
+
   {
     "artist": "Rosé and Bruno Mars",
     "title": "APT",
     "duration": "2 min 47",
-    "style": [
-      "Pop"
-    ],
+    "style": ["Pop"],
     "category": "2020s Music",
     "cover": "img/Rosé and Bruno Mars - APT.jpg"
   },
+
+  {
+    "artist": "Roxette",
+    "title": "Listen to your heart",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Roxette - Listen to your heart.jpg"
+  },
+
   {
     "artist": "Royksopp",
     "title": "Me and Youphoria",
     "duration": "4 min 25",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2020s Music",
     "cover": "img/Royksopp - Me and Youphoria.jpg"
   },
+
   {
     "artist": "Ryan Paris",
     "title": "La Dolce Vita",
     "duration": "3 min 23",
-    "style": [
-      "Disco"
-    ],
+    "style": ["Disco"],
     "category": "80s Music",
     "cover": "img/Ryan Paris - La Dolce Vita.jpg"
   },
+
   {
     "artist": "Ryuichi Sakamoto",
     "title": "Merry Christmas, Mr. Lawrence",
     "duration": "4 min 55",
-    "style": [
-      "Soundtrack"
-    ],
+    "style": ["Soundtrack"],
     "category": "80s Music",
     "cover": "img/Ryuichi Sakamoto - Merry Christmas, Mr. Lawrence.jpg"
   }
