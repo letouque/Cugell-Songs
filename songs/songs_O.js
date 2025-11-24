00ede3927e64665a8de4c4e4f4e5fff11520faba
letouque <letouque@gmail.com>
2025-11-24 19:45:44 +0100

    Update songs_O.js

--- songs/songs_O.js
+++ songs/songs_O.js
@@ -3,19 +3,25 @@ const songsO = [
     "artist": "Offspring",
     "title": "Pretty Fly",
     "duration": "3 min 02",
-    "style": [
-      "Punk Rock"
-    ],
+    "style": ["Punk Rock"],
     "category": "90s Music",
     "cover": "img/Offspring - Pretty Fly.jpg"
   },
+
+  {
+    "artist": "Outfield",
+    "title": "Your Love",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Outfield - Your Love.jpg"
+  },
+
   {
     "artist": "Outkast",
     "title": "Hey ya",
     "duration": "3 min 53",
-    "style": [
-      "Hip Hop"
-    ],
+    "style": ["Hip Hop"],
     "category": "2000s Music",
     "cover": "img/Outkast - Hey ya.jpg"
   }
