d3802ef7ea3f1d3ce7148434e46e1e821a51ebe2
letouque <letouque@gmail.com>
2025-11-24 19:18:02 +0100

    Update songs_E.js

--- songs/songs_E.js
+++ songs/songs_E.js
@@ -3,49 +3,68 @@ const songsE = [
     "artist": "Elvis Presley",
     "title": "Suspicious Minds",
     "duration": "3 min 25",
-    "style": [
-      "Rock"
-    ],
+    "style": ["Rock"],
     "category": "60s Music",
     "cover": "img/Elvis Presley - Suspicious Minds.jpg"
   },
+
+  {
+    "artist": "Eminem",
+    "title": "Lose Yourself",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Eminem - Lose Yourself.jpg"
+  },
+  {
+    "artist": "Eminem",
+    "title": "Stan",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Eminem - Stan.jpg"
+  },
+  {
+    "artist": "Eminem",
+    "title": "Whitout Me",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Eminem - Whitout Me.jpg"
+  },
+
   {
     "artist": "Ennio Moriconne",
     "title": "Chi Mai",
     "duration": "4 min 02",
-    "style": [
-      "Soundtrack"
-    ],
+    "style": ["Soundtrack"],
     "category": "70s Music",
     "cover": "img/Ennio Moriconne - Chi Mai.jpg"
   },
+
   {
     "artist": "Eurythmics",
     "title": "Here comes the rain again",
     "duration": "5 min 05",
-    "style": [
-      "Synthpop"
-    ],
+    "style": ["Synthpop"],
     "category": "80s Music",
     "cover": "img/Eurythmics - Here comes the rain again.jpg"
   },
+
   {
     "artist": "Evanescence",
     "title": "My immortal",
     "duration": "4 min 25",
-    "style": [
-      "Rock"
-    ],
+    "style": ["Rock"],
     "category": "2000s Music",
     "cover": "img/Evanescence - My immortal.jpg"
   },
+
   {
     "artist": "Everything But The Girl",
     "title": "Missing",
     "duration": "3 min 22",
-    "style": [
-      "Pop"
-    ],
+    "style": ["Pop"],
     "category": "90s Music",
     "cover": "img/Everything But The Girl - Missing.jpg"
   }
