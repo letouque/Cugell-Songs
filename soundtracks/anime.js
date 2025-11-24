2f9e492c048efe96fe36cc9ae53b5f664ca66c68
letouque <letouque@gmail.com>
2025-11-24 17:23:28 +0100

    Create anime.js

---
+++ soundtracks/anime.js
@@ -0,0 +1,132 @@
+const anime = [
+
+  /* ---------------------- NARUTO ---------------------- */
+  {
+    artist: "",
+    title: "Sadness and Sorrow",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Anime",
+    gameUniverse: "",
+    finalFantasy: "",
+    license: "Naruto",
+    style: ["Anime OST"],
+    cover: "img/Naruto - Sadness and Sorrow.jpg"
+  },
+  {
+    artist: "",
+    title: "Nightfall",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Anime",
+    gameUniverse: "",
+    finalFantasy: "",
+    license: "Naruto",
+    style: ["Anime OST"],
+    cover: "img/Naruto - Nightfall.jpg"
+  },
+  {
+    artist: "",
+    title: "Strong and Strike",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Anime",
+    gameUniverse: "",
+    finalFantasy: "",
+    license: "Naruto",
+    style: ["Anime OST"],
+    cover: "img/Naruto - Strong and Strike.jpg"
+  },
+  {
+    artist: "",
+    title: "Naruto Main Theme",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Anime",
+    gameUniverse: "",
+    finalFantasy: "",
+    license: "Naruto",
+    style: ["Anime OST"],
+    cover: "img/Naruto - Naruto Main Theme.jpg"
+  },
+
+  /* ---------------------- ATTACK ON TITAN ---------------------- */
+  {
+    artist: "",
+    title: "Call of Silence",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Anime",
+    gameUniverse: "",
+    finalFantasy: "",
+    license: "AOT",
+    style: ["Anime OST"],
+    cover: "img/AOT - Call of Silence.jpg"
+  },
+
+  /* ---------------------- FULLMETAL ALCHEMIST ---------------------- */
+  {
+    artist: "",
+    title: "Brothers",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Anime",
+    gameUniverse: "",
+    finalFantasy: "",
+    license: "FMA",
+    style: ["Anime OST"],
+    cover: "img/FMA - Brothers.jpg"
+  },
+
+  /* ---------------------- ONE PIECE ---------------------- */
+  {
+    artist: "",
+    title: "Bink's Sake",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Anime",
+    gameUniverse: "",
+    finalFantasy: "",
+    license: "One Piece",
+    style: ["Anime OST"],
+    cover: "img/One Piece - Bink's Sake.jpg"
+  },
+
+  /* ---------------------- PRINCESS MONONOKE ---------------------- */
+  {
+    artist: "",
+    title: "Journey to the West",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Anime",
+    gameUniverse: "",
+    finalFantasy: "",
+    license: "Mononoke",
+    style: ["Anime OST"],
+    cover: "img/Mononoke - Journey to the West.jpg"
+  },
+
+  /* ---------------------- KIMETSU NO YAIBA (DEMON SLAYER) ---------------------- */
+  {
+    artist: "",
+    title: "Tanjiro no Uta",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Anime",
+    gameUniverse: "",
+    finalFantasy: "",
+    license: "KNY",
+    style: ["Anime OST"],
+    cover: "img/KNY - Tanjiro no Uta.jpg"
+  }
+
+];
