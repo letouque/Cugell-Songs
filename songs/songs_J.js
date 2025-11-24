7d60e60ef18fcd0e0b0f418496b381a86f680738
letouque <letouque@gmail.com>
2025-11-24 19:27:04 +0100

    Update songs_J.js

--- songs/songs_J.js
+++ songs/songs_J.js
@@ -1,14 +1,6 @@
 const songsJ = [
   {
     "artist": "Jamiroquai",
-    "title": "Virtual Insanity",
-    "duration": "",
-    "style": [],
-    "category": "",
-    "cover": "img/Jamiroquai - Virtual Insanity.jpg"
-  },
-  {
-    "artist": "Jamiroquai",
     "title": "Deeper Underground",
     "duration": "",
     "style": [],
@@ -16,35 +8,59 @@ const songsJ = [
     "cover": "img/Jamiroquai - Deeper Underground.jpg"
   },
   {
+    "artist": "Jamiroquai",
+    "title": "Virtual Insanity",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Jamiroquai - Virtual Insanity.jpg"
+  },
+
+  {
     "artist": "Janet Jackson",
     "title": "Together again",
     "duration": "4 min 14",
-    "style": [
-      "Dance Pop"
-    ],
+    "style": ["Dance Pop"],
     "category": "90s Music",
     "cover": "img/Janet Jackson - Together again.jpg"
   },
+
+  {
+    "artist": "Jeanette",
+    "title": "Porque te vas",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Jeanette - Porque te vas.jpg"
+  },
+
   {
     "artist": "Jim Noir - My Patch",
     "title": "My Patch",
     "duration": "4 min 00",
-    "style": [
-      "Indie Pop"
-    ],
+    "style": ["Indie Pop"],
     "category": "2000s Music",
     "cover": "img/Jim Noir - My Patch - My Patch.jpg"
   },
+
+  {
+    "artist": "Jimmy Cliff",
+    "title": "Reggae Night",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Jimmy Cliff - Reggae Night.jpg"
+  },
+
   {
     "artist": "John Paul Young",
     "title": "Love Is In The Air",
     "duration": "3 min 07",
-    "style": [
-      "Disco"
-    ],
+    "style": ["Disco"],
     "category": "70s Music",
     "cover": "img/John Paul Young - Love Is In The Air.jpg"
   },
+
   {
     "artist": "Johnny Cash",
     "title": "Hurt",
@@ -53,23 +69,38 @@ const songsJ = [
     "category": "",
     "cover": "img/Johnny Cash - Hurt.jpg"
   },
+
   {
     "artist": "Johnny Hallyday",
     "title": "Allumer le feu",
     "duration": "5 min 23",
-    "style": [
-      "French Rock"
-    ],
+    "style": ["French Rock"],
     "category": "90s Music",
     "cover": "img/Johnny Hallyday - Allumer le feu.jpg"
   },
   {
+    "artist": "Johnny Hallyday",
+    "title": "L'envie",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Johnny Hallyday - L'envie.jpg"
+  },
+
+  {
+    "artist": "Joy Division",
+    "title": "Love will tear us apart",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Joy Division - Love will tear us apart.jpg"
+  },
+
+  {
     "artist": "Justice",
     "title": "Civilization",
     "duration": "4 min 09",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2010s Music",
     "cover": "img/Justice - Civilization.jpg"
   },
@@ -77,9 +108,7 @@ const songsJ = [
     "artist": "Justice",
     "title": "D.A.N.C.E",
     "duration": "2 min 59",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2000s Music",
     "cover": "img/Justice - D.A.N.C.E.jpg"
   },
@@ -87,19 +116,16 @@ const songsJ = [
     "artist": "Justice",
     "title": "On N On",
     "duration": "3 min 13",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2010s Music",
     "cover": "img/Justice - On N On.jpg"
   },
+
   {
     "artist": "Justice & Simian",
     "title": "We Are Your Friends",
     "duration": "2 min 40",
-    "style": [
-      "Electronic"
-    ],
+    "style": ["Electronic"],
     "category": "2000s Music",
     "cover": "img/Justice & Simian - We Are Your Friends.jpg"
   }
