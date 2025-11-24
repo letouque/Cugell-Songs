be6b173df348f393aad95eca5cd1c99ca3d251cf
letouque <letouque@gmail.com>
2025-11-24 19:44:53 +0100

    Update songs_N.js

--- songs/songs_N.js
+++ songs/songs_N.js
@@ -3,49 +3,61 @@ const songsN = [
     "artist": "Nathalie Cardone",
     "title": "Hasta Siempre",
     "duration": "4 min 12",
-    "style": [
-      "Latin Folk"
-    ],
+    "style": ["Latin Folk"],
     "category": "90s Music",
     "cover": "img/Nathalie Cardone - Hasta Siempre.jpg"
   },
+
   {
     "artist": "New Order",
     "title": "Blue Monday",
     "duration": "7 min 34",
-    "style": [
-      "Synthpop"
-    ],
+    "style": ["Synthpop"],
     "category": "80s Music",
     "cover": "img/New Order - Blue Monday.jpg"
   },
+
   {
     "artist": "Nick Cave & the Bad Seeds",
     "title": "Red Right Hand",
     "duration": "6 min 02",
-    "style": [
-      "Alternative Rock"
-    ],
+    "style": ["Alternative Rock"],
     "category": "90s Music",
     "cover": "img/Nick Cave & the Bad Seeds - Red Right Hand.jpg"
   },
+
+  {
+    "artist": "Nino Ferrer",
+    "title": "Le sud",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Nino Ferrer - Le sud.jpg"
+  },
+
   {
     "artist": "Nik Kershaw",
     "title": "Wouldn't it be good",
     "duration": "4 min 16",
-    "style": [
-      "New Wave"
-    ],
+    "style": ["New Wave"],
     "category": "80s Music",
     "cover": "img/Nik Kershaw - Wouldn't it be good.jpg"
   },
+
+  {
+    "artist": "No Mercy",
+    "title": "Where do you go",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/No Mercy - Where do you go.jpg"
+  },
+
   {
     "artist": "NSync",
     "title": "Bye Bye Bye",
     "duration": "3 min 20",
-    "style": [
-      "Pop"
-    ],
+    "style": ["Pop"],
     "category": "2000s Music",
     "cover": "img/NSync - Bye Bye Bye.jpg"
   }
