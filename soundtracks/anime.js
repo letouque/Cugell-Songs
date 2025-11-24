4dc7b8149f0703922c6f294a403606b0427c2096
letouque <letouque@gmail.com>
2025-11-24 21:34:40 +0100

    Update anime.js

--- soundtracks/anime.js
+++ soundtracks/anime.js
@@ -1,63 +1,7 @@
 const anime = [
-
-  /* ---------------------- NARUTO ---------------------- */
   {
     artist: "",
-    title: "Sadness and Sorrow",
-    duration: "",
-    year: "",
-    category: "",
-    mediaType: "Anime",
-    gameUniverse: "",
-    finalFantasy: "",
-    license: "Naruto",
-    style: ["Anime OST"],
-    cover: "img/Naruto - Sadness and Sorrow.jpg"
-  },
-  {
-    artist: "",
-    title: "Nightfall",
-    duration: "",
-    year: "",
-    category: "",
-    mediaType: "Anime",
-    gameUniverse: "",
-    finalFantasy: "",
-    license: "Naruto",
-    style: ["Anime OST"],
-    cover: "img/Naruto - Nightfall.jpg"
-  },
-  {
-    artist: "",
-    title: "Strong and Strike",
-    duration: "",
-    year: "",
-    category: "",
-    mediaType: "Anime",
-    gameUniverse: "",
-    finalFantasy: "",
-    license: "Naruto",
-    style: ["Anime OST"],
-    cover: "img/Naruto - Strong and Strike.jpg"
-  },
-  {
-    artist: "",
-    title: "Naruto Main Theme",
-    duration: "",
-    year: "",
-    category: "",
-    mediaType: "Anime",
-    gameUniverse: "",
-    finalFantasy: "",
-    license: "Naruto",
-    style: ["Anime OST"],
-    cover: "img/Naruto - Naruto Main Theme.jpg"
-  },
-
-  /* ---------------------- ATTACK ON TITAN ---------------------- */
-  {
-    artist: "",
-    title: "Call of Silence",
+    title: "Warrior",
     duration: "",
     year: "",
     category: "",
@@ -66,10 +10,21 @@ const anime = [
     finalFantasy: "",
     license: "AOT",
     style: ["Anime OST"],
-    cover: "img/AOT - Call of Silence.jpg"
+    cover: "img/AOT - Warrior.jpg"
   },
-
-  /* ---------------------- FULLMETAL ALCHEMIST ---------------------- */
+  {
+    artist: "",
+    title: "Prologue",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Anime",
+    gameUniverse: "",
+    finalFantasy: "",
+    license: "DBZ",
+    style: ["Anime OST"],
+    cover: "img/DBZ - Prologue.jpg"
+  },
   {
     artist: "",
     title: "Brothers",
@@ -83,8 +38,32 @@ const anime = [
     style: ["Anime OST"],
     cover: "img/FMA - Brothers.jpg"
   },
-
-  /* ---------------------- ONE PIECE ---------------------- */
+  {
+    artist: "",
+    title: "Kamado Tanjiro no Uta",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Anime",
+    gameUniverse: "",
+    finalFantasy: "",
+    license: "KNY",
+    style: ["Anime OST"],
+    cover: "img/KNY - Kamado Tanjiro no Uta.jpg"
+  },
+  {
+    artist: "",
+    title: "Main Theme",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Anime",
+    gameUniverse: "",
+    finalFantasy: "",
+    license: "Naruto",
+    style: ["Anime OST"],
+    cover: "img/Naruto - Main Theme.jpg"
+  },
   {
     artist: "",
     title: "Bink's Sake",
@@ -98,11 +77,9 @@ const anime = [
     style: ["Anime OST"],
     cover: "img/One Piece - Bink's Sake.jpg"
   },
-
-  /* ---------------------- PRINCESS MONONOKE ---------------------- */
   {
     artist: "",
-    title: "Journey to the West",
+    title: "Journey To The West",
     duration: "",
     year: "",
     category: "",
@@ -111,22 +88,6 @@ const anime = [
     finalFantasy: "",
     license: "Mononoke",
     style: ["Anime OST"],
-    cover: "img/Mononoke - Journey to the West.jpg"
-  },
-
-  /* ---------------------- KIMETSU NO YAIBA (DEMON SLAYER) ---------------------- */
-  {
-    artist: "",
-    title: "Tanjiro no Uta",
-    duration: "",
-    year: "",
-    category: "",
-    mediaType: "Anime",
-    gameUniverse: "",
-    finalFantasy: "",
-    license: "KNY",
-    style: ["Anime OST"],
-    cover: "img/KNY - Tanjiro no Uta.jpg"
+    cover: "img/Mononoke - Journey To The West.jpg"
   }
-
 ];
