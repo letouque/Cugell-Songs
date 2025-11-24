b1b3bfb6b4afda9a6c94cdf8d6da6fb0275a435a
letouque <letouque@gmail.com>
2025-11-24 22:15:45 +0100

    Create ff14.js

---
+++ soundtracks/ff14.js
@@ -0,0 +1,62 @@
+const ff14 = [
+  {
+    artist: "Masayoshi Soken",
+    title: "Course Uncharted",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Game",
+    gameUniverse: "Final Fantasy XIV",
+    finalFantasy: "XIV",
+    style: ["Game OST"],
+    cover: "img/Final Fantasy XIV - Cover.jpg"
+  },
+  {
+    artist: "Masayoshi Soken",
+    title: "Dragonsong Rock",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Game",
+    gameUniverse: "Final Fantasy XIV",
+    finalFantasy: "XIV",
+    style: ["Game OST"],
+    cover: "img/Final Fantasy XIV - Cover.jpg"
+  },
+  {
+    artist: "Masayoshi Soken",
+    title: "Onsal Hakair (PVP)",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Game",
+    gameUniverse: "Final Fantasy XIV",
+    finalFantasy: "XIV",
+    style: ["Game OST"],
+    cover: "img/Final Fantasy XIV - Cover.jpg"
+  },
+  {
+    artist: "Masayoshi Soken",
+    title: "Rythm of the Realm",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Game",
+    gameUniverse: "Final Fantasy XIV",
+    finalFantasy: "XIV",
+    style: ["Game OST"],
+    cover: "img/Final Fantasy XIV - Cover.jpg"
+  },
+  {
+    artist: "Masayoshi Soken",
+    title: "Tomorrow and Tomorrow",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Game",
+    gameUniverse: "Final Fantasy XIV",
+    finalFantasy: "XIV",
+    style: ["Game OST"],
+    cover: "img/Final Fantasy XIV - Cover.jpg"
+  }
+];
