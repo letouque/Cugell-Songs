c4e04a22b256ceab30f5f3903c5590f8d1fab5e2
letouque <letouque@gmail.com>
2025-11-24 17:24:11 +0100

    Create castevania.js

---
+++ soundtracks/castevania.js
@@ -0,0 +1,15 @@
+const castlevania = [
+  {
+    artist: "",
+    title: "From Castlevania",
+    duration: "",
+    year: "",
+    category: "",
+    mediaType: "Game",
+    gameUniverse: "Sega / Old School",
+    finalFantasy: "",
+    license: "Castlevania",
+    style: ["Game OST"],
+    cover: "img/Castlevania - From Castlevania.jpg"
+  }
+];
