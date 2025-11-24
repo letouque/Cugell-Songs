3163a8451e65c4a1f5b50eb723fad906b3403080
letouque <letouque@gmail.com>
2025-11-24 19:22:13 +0100

    Update songs_G.js

--- songs/songs_G.js
+++ songs/songs_G.js
@@ -3,29 +3,52 @@ const songsG = [
     "artist": "Game of Thrones",
     "title": "Light of The Seven",
     "duration": "9 min 41",
-    "style": [
-      "Soundtrack"
-    ],
+    "style": ["Soundtrack"],
     "category": "2010s Music",
     "cover": "img/Game of Thrones - Light of The Seven.jpg"
   },
+
+  {
+    "artist": "Gary Moore",
+    "title": "Parisienne Walkways",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Gary Moore - Parisienne Walkways.jpg"
+  },
+
   {
     "artist": "Gerry Rafferty",
     "title": "Baker Street",
     "duration": "6 min 17",
-    "style": [
-      "Soft Rock"
-    ],
+    "style": ["Soft Rock"],
     "category": "70s Music",
     "cover": "img/Gerry Rafferty - Baker Street.jpg"
   },
+
+  {
+    "artist": "Giorgio Moroder & Phil Oakey",
+    "title": "Together In Electric Dreams",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Giorgio Moroder & Phil Oakey - Together In Electric Dreams.jpg"
+  },
+
+  {
+    "artist": "Gloria Gaynor",
+    "title": "Can't take my eyes of you",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Gloria Gaynor - Can't take my eyes of you.jpg"
+  },
+
   {
     "artist": "Gorillaz",
     "title": "Baby Queen",
     "duration": "3 min 24",
-    "style": [
-      "Alternative"
-    ],
+    "style": ["Alternative"],
     "category": "2020s Music",
     "cover": "img/Gorillaz - Baby Queen.jpg"
   },
@@ -33,9 +56,7 @@ const songsG = [
     "artist": "Gorillaz",
     "title": "Clint Eastwood",
     "duration": "4 min 11",
-    "style": [
-      "Alternative"
-    ],
+    "style": ["Alternative"],
     "category": "2000s Music",
     "cover": "img/Gorillaz - Clint Eastwood.jpg"
   },
@@ -43,30 +64,43 @@ const songsG = [
     "artist": "Gorillaz",
     "title": "Silent Running",
     "duration": "4 min 20",
-    "style": [
-      "Alternative"
-    ],
+    "style": ["Alternative"],
     "category": "2020s Music",
     "cover": "img/Gorillaz - Silent Running.jpg"
   },
+
   {
     "artist": "Gotye",
     "title": "Somebody That I Used To Know ft. Kimbra",
     "duration": "4 min 05",
-    "style": [
-      "Indie Pop"
-    ],
+    "style": ["Indie Pop"],
     "category": "2010s Music",
     "cover": "img/Gotye - Somebody That I Used To Know ft. Kimbra.jpg"
   },
+
   {
     "artist": "Green Day",
     "title": "Boulevard of Broken Dreams",
     "duration": "4 min 17",
-    "style": [
-      "Alternative Rock"
-    ],
+    "style": ["Alternative Rock"],
     "category": "2000s Music",
     "cover": "img/Green Day - Boulevard of Broken Dreams.jpg"
+  },
+
+  {
+    "artist": "Guns and Roses",
+    "title": "November Rain",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Guns and Roses - November Rain.jpg"
+  },
+  {
+    "artist": "Guns and Roses",
+    "title": "Welcome to the Jungle",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Guns and Roses - Welcome to the Jungle.jpg"
   }
 ];
