92ba4840f9f24d0e3ddbb776d8e1fb21d901e055
letouque <letouque@gmail.com>
2025-11-24 21:20:50 +0100

    Update songs_W.js

--- songs/songs_W.js
+++ songs/songs_W.js
@@ -3,9 +3,7 @@ const songsW = [
     "artist": "Weezer",
     "title": "Buddy Holly",
     "duration": "2 min 46",
-    "style": [
-      "Alternative Rock"
-    ],
+    "style": ["Alternative Rock"],
     "category": "90s Music",
     "cover": "img/Weezer - Buddy Holly.jpg"
   },
@@ -13,9 +11,7 @@ const songsW = [
     "artist": "Weezer",
     "title": "Only in dreams",
     "duration": "7 min 57",
-    "style": [
-      "Alternative Rock"
-    ],
+    "style": ["Alternative Rock"],
     "category": "90s Music",
     "cover": "img/Weezer - Only in dreams.jpg"
   },
@@ -23,12 +19,11 @@ const songsW = [
     "artist": "Weezer",
     "title": "Undone",
     "duration": "4 min 32",
-    "style": [
-      "Alternative Rock"
-    ],
+    "style": ["Alternative Rock"],
     "category": "90s Music",
     "cover": "img/Weezer - Undone.jpg"
   },
+
   {
     "artist": "Wet Wet Wet",
     "title": "Love is all around",
@@ -36,5 +31,14 @@ const songsW = [
     "style": [],
     "category": "",
     "cover": "img/Wet Wet Wet - Love is all around.jpg"
+  },
+
+  {
+    "artist": "Wham!",
+    "title": "Last Christmas",
+    "duration": "",
+    "style": [],
+    "category": "",
+    "cover": "img/Wham! - Last Christmas.jpg"
   }
 ];
