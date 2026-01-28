8014ce42fdccd9751adbff36444d605bf1c5be51
letouque <letouque@gmail.com>
2026-01-28 14:30:30 +0100

    Add files via upload

--- songs/songs_A.js
+++ songs/songs_A.js
@@ -2,209 +2,325 @@ const songsA = [
   {
     "artist": "ABBA",
     "title": "Gimme ! Gimme ! Gimme !",
-    "duration": "3 min 07",
-    "style": ["Pop","Disco"],
+    "duration": "00:00",
+    "year": "1979",
+    "style": [
+      "Disco",
+      "Pop"
+    ],
     "category": "70s Music",
-    "cover": "img/ABBA - Gimme.jpg"
+    "cover": "img/ABBA - Gimme.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "ABBA",
     "title": "Mammamia",
     "duration": "3 min 28",
-    "style": ["Pop","Disco"],
+    "year": "",
+    "style": [
+      "Pop",
+      "Disco"
+    ],
     "category": "70s Music",
-    "cover": "img/ABBA - Mammamia.jpg"
+    "cover": "img/ABBA - Mammamia.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
+  },
+  {
+    "artist": "AC/DC",
+    "title": "Back in Black",
+    "duration": "00:00",
+    "year": "1980",
+    "style": [
+      "Hard Rock",
+      "Rock"
+    ],
+    "category": "80s Music",
+    "cover": "img/ACDC - Back in Black.jpg",
+    "addedAt": "2026-01-28T13:27:53.656Z"
   },
   {
     "artist": "Ace Of Base",
     "title": "All that she wants",
     "duration": "3 min 22",
-    "style": ["Pop","Dance"],
+    "year": "",
+    "style": [
+      "Pop",
+      "Dance"
+    ],
     "category": "90s Music",
-    "cover": "img/Ace Of Base - All that she wants.jpg"
+    "cover": "img/Ace Of Base - All that she wants.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Ace Of Base",
     "title": "Happy Nation",
     "duration": "3 min 36",
-    "style": ["Pop"],
+    "year": "",
+    "style": [
+      "Pop"
+    ],
     "category": "90s Music",
-    "cover": "img/Ace Of Base - Happy Nation.jpg"
+    "cover": "img/Ace Of Base - Happy Nation.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Ace Of Base",
     "title": "The Sign",
     "duration": "3 min 06",
-    "style": ["Pop"],
+    "year": "",
+    "style": [
+      "Pop"
+    ],
     "category": "90s Music",
-    "cover": "img/Ace Of Base - The Sign.jpg"
+    "cover": "img/Ace Of Base - The Sign.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Aha",
     "title": "Take on me",
     "duration": "3 min 42",
-    "style": ["Pop"],
+    "year": "",
+    "style": [
+      "Pop"
+    ],
     "category": "80s Music",
-    "cover": "img/Aha - Take on me.jpg"
+    "cover": "img/Aha - Take on me.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Air",
     "title": "Kelly watch the stars",
     "duration": "3 min 42",
-    "style": ["Electronic"],
+    "year": "",
+    "style": [
+      "Electronic"
+    ],
     "category": "90s Music",
-    "cover": "img/Air - Kelly watch the stars.jpg"
-  },
-  {
-    "artist": "Alannah Myles",
-    "title": "Black Velvet",
-    "duration": "4 min 39",
-    "style": ["Rock"],
-    "category": "80s Music",
-    "cover": "img/Alannah Myles - Black Velvet.jpg"
+    "cover": "img/Air - Kelly watch the stars.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Alan Parson's Project",
     "title": "Eye in the sky",
     "duration": "4 min 25",
-    "style": ["Rock"],
+    "year": "",
+    "style": [
+      "Rock"
+    ],
     "category": "80s Music",
-    "cover": "img/Alan Parson - Eye in the sky.jpg"
+    "cover": "img/Alan Parson - Eye in the sky.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
+  },
+  {
+    "artist": "Alannah Myles",
+    "title": "Black Velvet",
+    "duration": "4 min 39",
+    "year": "",
+    "style": [
+      "Rock"
+    ],
+    "category": "80s Music",
+    "cover": "img/Alannah Myles - Black Velvet.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Alice Deejay",
     "title": "Better off alone",
     "duration": "3 min 32",
-    "style": ["Dance"],
+    "year": "",
+    "style": [
+      "Dance"
+    ],
     "category": "90s Music",
-    "cover": "img/Alice Deejay - Better off alone.jpg"
+    "cover": "img/Alice Deejay - Better off alone.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Alicia Keys",
     "title": "No one",
     "duration": "4 min 08",
-    "style": ["R","B"],
+    "year": "",
+    "style": [],
     "category": "2000s Music",
-    "cover": "img/Alicia Keys - No one.jpg"
+    "cover": "img/Alicia Keys - No one.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Alizee",
     "title": "Moi Lolita",
     "duration": "4 min 26",
-    "style": ["French Pop"],
+    "year": "",
+    "style": [
+      "French Pop"
+    ],
     "category": "2000s Music",
-    "cover": "img/Alizee - Moi Lolita.jpg"
+    "cover": "img/Alizee - Moi Lolita.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "All Saints",
     "title": "Lady Marmalade",
     "duration": "",
-    "style": ["Pop"],
+    "year": "",
+    "style": [
+      "Pop"
+    ],
     "category": "2000s Music",
-    "cover": "img/All Saints - Lady Marmalade.jpg"
+    "cover": "img/All Saints - Lady Marmalade.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "All Saints",
     "title": "Pure Shores",
     "duration": "4 min 27",
-    "style": ["Pop"],
+    "year": "",
+    "style": [
+      "Pop"
+    ],
     "category": "2000s Music",
-    "cover": "img/All Saints - Pure Shores.jpg"
+    "cover": "img/All Saints - Pure Shores.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Alphaville",
     "title": "Big in Japan",
     "duration": "4 min 46",
-    "style": ["New Wave"],
+    "year": "",
+    "style": [
+      "New Wave"
+    ],
     "category": "80s Music",
-    "cover": "img/Alphaville - Big in Japan.jpg"
+    "cover": "img/Alphaville - Big in Japan.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Alphaville",
     "title": "Forever Young",
     "duration": "3 min 49",
-    "style": ["New Wave"],
+    "year": "",
+    "style": [
+      "New Wave"
+    ],
     "category": "80s Music",
-    "cover": "img/Alphaville - Forever Young.jpg"
+    "cover": "img/Alphaville - Forever Young.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Amy Winehouse",
     "title": "Back to Black",
     "duration": "3 min 56",
-    "style": ["Soul"],
+    "year": "",
+    "style": [
+      "Soul"
+    ],
     "category": "2000s Music",
-    "cover": "img/Amy Winehouse - Back to Black.jpg"
+    "cover": "img/Amy Winehouse - Back to Black.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Amy Winehouse",
     "title": "You know I'm no good",
     "duration": "3 min 34",
-    "style": ["Soul"],
+    "year": "",
+    "style": [
+      "Soul"
+    ],
     "category": "2000s Music",
-    "cover": "img/Amy Winehouse - You know Im no good.jpg"
+    "cover": "img/Amy Winehouse - You know Im no good.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Andrea Bocelli",
     "title": "Vivo por Ella",
     "duration": "",
+    "year": "",
     "style": [],
     "category": "",
-    "cover": "img/Andrea Bocelli - Vivo per Lei.jpg"
+    "cover": "img/Andrea Bocelli - Vivo per Lei.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Animals",
     "title": "House of the rising sun",
     "duration": "4 min 39",
-    "style": ["Rock"],
+    "year": "",
+    "style": [
+      "Rock"
+    ],
     "category": "60s Music",
-    "cover": "img/Animals - House of the rising sun.jpg"
+    "cover": "img/Animals - House of the rising sun.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Anita Ward",
     "title": "Ring My Bell",
     "duration": "3 min 26",
-    "style": ["Disco"],
+    "year": "",
+    "style": [
+      "Disco"
+    ],
     "category": "70s Music",
-    "cover": "img/Anita Ward - Ring My Bell.jpg"
+    "cover": "img/Anita Ward - Ring My Bell.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Ann Lee",
     "title": "2 Times",
     "duration": "3 min 48",
-    "style": ["Dance-Pop"],
+    "year": "",
+    "style": [
+      "Dance-Pop"
+    ],
     "category": "90s Music",
-    "cover": "img/Ann Lee - 2 Times.jpg"
+    "cover": "img/Ann Lee - 2 Times.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Arctic Monkeys",
     "title": "505",
     "duration": "4 min 10",
-    "style": ["Indie Rock"],
+    "year": "",
+    "style": [
+      "Indie Rock"
+    ],
     "category": "2000s Music",
-    "cover": "img/Arctic Monkeys - 505.jpg"
+    "cover": "img/Arctic Monkeys - 505.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Artemas",
     "title": "I like the way you kiss me",
     "duration": "2 min 18",
-    "style": ["Pop"],
+    "year": "",
+    "style": [
+      "Pop"
+    ],
     "category": "2020s Music",
-    "cover": "img/Artemas - I like the way you kiss me.jpg"
+    "cover": "img/Artemas - I like the way you kiss me.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Avicii",
     "title": "Levels",
     "duration": "2 min 57",
-    "style": ["Electronic"],
+    "year": "",
+    "style": [
+      "Electronic"
+    ],
     "category": "2010s Music",
-    "cover": "img/Avicii - Levels.jpg"
+    "cover": "img/Avicii - Levels.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   },
   {
     "artist": "Awolnation",
     "title": "Sail",
     "duration": "4 min 00",
-    "style": ["Alternative Rock"],
+    "year": "",
+    "style": [
+      "Alternative Rock"
+    ],
     "category": "2010s Music",
-    "cover": "img/Awolnation - Sail.jpg"
+    "cover": "img/Awolnation - Sail.jpg",
+    "addedAt": "2026-01-28T13:26:40.824Z"
   }
 ];
