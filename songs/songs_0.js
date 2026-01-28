a1e79ea548cfb829d0e84262b1fa9bef0acbd6d1
letouque <letouque@gmail.com>
2026-01-28 17:03:01 +0100

    Add files via upload

--- songs/songs_0.js
+++ songs/songs_0.js
@@ -2,9 +2,25 @@ const songs09 = [
   {
     "artist": "10cc",
     "title": "I'm not in love",
-    "duration": "5 min 06",
-    "style": ["Soft Rock"],
+    "duration": "00:00",
+    "year": "1975",
+    "style": [
+      "Soft Rock"
+    ],
     "category": "70s Music",
-    "cover": "img/10cc - Im not in love.jpg"
+    "cover": "img/10cc - Im not in love.jpg",
+    "addedAt": "2026-01-28T15:53:55.015Z"
+  },
+  {
+    "artist": "50 Cent",
+    "title": "Candy Shop",
+    "duration": "00:00",
+    "year": "2005",
+    "style": [
+      "Hip Hop"
+    ],
+    "category": "2000s Music",
+    "cover": "img/50 Cent - Candy Shop.jpg",
+    "addedAt": "2026-01-28T15:54:48.377Z"
   }
 ];
