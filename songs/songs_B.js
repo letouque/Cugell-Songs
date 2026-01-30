62bdac5dea614c159791f865e889b30aa7e1eebb
letouque <letouque@gmail.com>
2026-01-30 16:17:20 +0100

    Add files via upload

--- songs/songs_B.js
+++ songs/songs_B.js
@@ -1,224 +1,285 @@
 const songsB = [
   {
-    "artist": "Backstreet Boys",
-    "title": "I Want It That Way",
-    "duration": "3 min 29",
-    "style": [
-      "Pop"
-    ],
-    "category": "90s Music",
-    "cover": "img/Backstreet Boys - I Want It That Way.jpg"
-  },
-  {
-    "artist": "Bag Raiders",
-    "title": "Shooting Star",
-    "duration": "3 min 50",
-    "style": [
-      "Electronic"
-    ],
-    "category": "2000s Music",
-    "cover": "img/Bag Raiders - Shooting Star.jpg"
-  },
-  {
-  "artist": "B-52s",
-  "title": "Love Shack",
-  "duration": "5 min 21",
-  "style": ["Pop Rock"],
-  "category": "80s Music",
-  "cover": "img/B-52s - Love Shack.jpg"
-},
- {
- "artist": "Babymetal",
- "title": "Gimme Chocolate",
- "duration": "3 min 51",
-  "style": ["Metal", "J-Pop"],
-  "category": "2010s Music",
-  "cover": "img/Babymetal - Gimme Chocolate.jpg"
-},
-  {
-    "artist": "Bangles",
-    "title": "Eternal Flame",
-    "duration": "3 min 47",
-    "style": [
-      "Pop"
-    ],
+    "artist": "",
+    "title": "",
+    "duration": "",
+    "year": "",
+    "style": [],
     "category": "80s Music",
-    "cover": "img/Bangles - Eternal Flame.jpg"
+    "cover": "img/default-cover.jpg",
+    "addedAt": "2026-01-30T15:14:48.590Z"
   },
   {
-    "artist": "Bangles",
-    "title": "Walk like an Egyptian",
-    "duration": "3 min 12",
+    "artist": "B-52s",
+    "title": "Love Shack",
+    "duration": "05:21",
+    "year": "",
     "style": [
       "Pop Rock"
     ],
     "category": "80s Music",
-    "cover": "img/Bangles - Walk like an Egyptian.jpg"
+    "cover": "img/B-52s - Love Shack.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
+  },
+  {
+    "artist": "Babymetal",
+    "title": "Gimme Chocolate",
+    "duration": "03:51",
+    "year": "",
+    "style": [
+      "Metal",
+      "J-Pop"
+    ],
+    "category": "2010s Music",
+    "cover": "img/Babymetal - Gimme Chocolate.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
+  },
+  {
+    "artist": "Backstreet Boys",
+    "title": "I Want It That Way",
+    "duration": "03:29",
+    "year": "",
+    "style": [
+      "Pop"
+    ],
+    "category": "90s Music",
+    "cover": "img/Backstreet Boys - I Want It That Way.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
+  },
+  {
+    "artist": "Bag Raiders",
+    "title": "Shooting Star",
+    "duration": "03:50",
+    "year": "",
+    "style": [
+      "Electronic"
+    ],
+    "category": "2000s Music",
+    "cover": "img/Bag Raiders - Shooting Star.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
+  },
+  {
+    "artist": "Bangles",
+    "title": "Eternal Flame",
+    "duration": "03:47",
+    "year": "",
+    "style": [
+      "Pop"
+    ],
+    "category": "80s Music",
+    "cover": "img/Bangles - Eternal Flame.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
+  },
+  {
+    "artist": "Bangles",
+    "title": "Walk like an Egyptian",
+    "duration": "03:12",
+    "year": "",
+    "style": [
+      "Pop Rock"
+    ],
+    "category": "80s Music",
+    "cover": "img/Bangles - Walk like an Egyptian.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Barry White",
     "title": "You're the first the last my everything",
-    "duration": "3 min 37",
+    "duration": "03:37",
+    "year": "",
     "style": [
       "Soul"
     ],
     "category": "70s Music",
-    "cover": "img/Barry White - Youre the first the last my everything.jpg"
+    "cover": "img/Barry White - Youre the first the last my everything.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Billie Eilish",
     "title": "Bad Guy",
-    "duration": "3 min 11",
+    "duration": "03:11",
+    "year": "",
     "style": [
       "Pop"
     ],
     "category": "2010s Music",
-    "cover": "img/Billie Eilish - Bad Guy.jpg"
+    "cover": "img/Billie Eilish - Bad Guy.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Billie Eilish",
     "title": "Birds of a feather",
-    "duration": "3 min 25",
+    "duration": "03:25",
+    "year": "",
     "style": [
       "Pop"
     ],
     "category": "2020s Music",
-    "cover": "img/Billie Eilish - Birds of a feather.jpg"
+    "cover": "img/Billie Eilish - Birds of a feather.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Billy Idol",
     "title": "Eyes Without A Face",
-    "duration": "5 min 01",
+    "duration": "05:01",
+    "year": "",
     "style": [
       "Rock"
     ],
     "category": "80s Music",
-    "cover": "img/Billy Idol - Eyes Without A Face.jpg"
+    "cover": "img/Billy Idol - Eyes Without A Face.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Billy Idol",
     "title": "Flesh for Fantasy",
-    "duration": "4 min 05",
+    "duration": "04:05",
+    "year": "",
     "style": [
       "Rock"
     ],
     "category": "80s Music",
-    "cover": "img/Billy Idol - Flesh for Fantasy.jpg"
+    "cover": "img/Billy Idol - Flesh for Fantasy.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Billy Idol",
     "title": "White Wedding",
-    "duration": "3 min 21",
+    "duration": "03:21",
+    "year": "",
     "style": [
       "Rock"
     ],
     "category": "80s Music",
-    "cover": "img/Billy Idol - White Wedding.jpg"
+    "cover": "img/Billy Idol - White Wedding.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Black",
     "title": "Wonderful Life",
-    "duration": "4 min 42",
+    "duration": "04:42",
+    "year": "",
     "style": [
       "Pop"
     ],
     "category": "80s Music",
-    "cover": "img/Black - Wonderful Life.jpg"
+    "cover": "img/Black - Wonderful Life.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Black Eyed Peas",
     "title": "I got a feeling",
-    "duration": "4 min 57",
+    "duration": "04:57",
+    "year": "",
     "style": [
       "Dance-Pop"
     ],
     "category": "2000s Music",
-    "cover": "img/Black Eyed Peas - I got a feeling.jpg"
+    "cover": "img/Black Eyed Peas - I got a feeling.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Black Eyed Peas",
     "title": "Meet me halfway",
-    "duration": "4 min 42",
+    "duration": "04:42",
+    "year": "",
     "style": [
       "Pop"
     ],
     "category": "2000s Music",
-    "cover": "img/Black Eyed Peas - Meet me halfway.jpg"
+    "cover": "img/Black Eyed Peas - Meet me halfway.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Black Eyed Peas",
     "title": "Pump It",
-    "duration": "3 min 32",
+    "duration": "03:32",
+    "year": "",
     "style": [
       "Hip Hop"
     ],
     "category": "2000s Music",
-    "cover": "img/Black Eyed Peas - Pump It.jpg"
+    "cover": "img/Black Eyed Peas - Pump It.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Blondie",
     "title": "Atomic",
-    "duration": "4 min 59",
+    "duration": "04:59",
+    "year": "",
     "style": [
       "New Wave"
     ],
     "category": "80s Music",
-    "cover": "img/Blondie - Atomic.jpg"
+    "cover": "img/Blondie - Atomic.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Blondie",
     "title": "Heart of glass",
-    "duration": "4 min 19",
+    "duration": "04:19",
+    "year": "",
     "style": [
       "New Wave"
     ],
     "category": "70s Music",
-    "cover": "img/Blondie - Heart of glass.jpg"
+    "cover": "img/Blondie - Heart of glass.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Blues Brothers",
     "title": "Peter Gunn Theme",
     "duration": "",
+    "year": "",
     "style": [],
     "category": "",
-    "cover": "img/Blues Brothers - Peter Gunn Theme.jpg"
+    "cover": "img/Blues Brothers - Peter Gunn Theme.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Bon Jovi",
     "title": "It's my life",
     "duration": "",
+    "year": "",
     "style": [],
     "category": "",
-    "cover": "img/Bon Jovi - Its my life.jpg"
+    "cover": "img/Bon Jovi - Its my life.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Boney M",
     "title": "Rasputin",
-    "duration": "4 min 25",
+    "duration": "04:25",
+    "year": "",
     "style": [
       "Disco"
     ],
     "category": "70s Music",
-    "cover": "img/Boney M - Rasputin.jpg"
+    "cover": "img/Boney M - Rasputin.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "Bonnie Tyler",
     "title": "Holding outta hero",
-    "duration": "5 min 09",
+    "duration": "05:09",
+    "year": "",
     "style": [
       "Rock"
     ],
     "category": "80s Music",
-    "cover": "img/Bonnie Tyler - Holding outta hero.jpg"
+    "cover": "img/Bonnie Tyler - Holding outta hero.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   },
   {
     "artist": "BTS",
     "title": "Standing Next to You",
-    "duration": "3 min 19",
+    "duration": "03:19",
+    "year": "",
     "style": [
       "K-Pop"
     ],
     "category": "2020s Music",
-    "cover": "img/BTS - Standing Next to You.jpg"
+    "cover": "img/BTS - Standing Next to You.jpg",
+    "addedAt": "2026-01-30T15:14:05.675Z"
   }
 ];
