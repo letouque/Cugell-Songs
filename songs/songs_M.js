4b1232cfdbd18f2ad0495eb20fc95be0bdf4d893
letouque <letouque@gmail.com>
2026-02-02 04:09:46 +0100

    Update songs/songs_M.js

--- songs/songs_M.js
+++ songs/songs_M.js
@@ -2,237 +2,321 @@ const songsM = [
   {
     "artist": "Macklemore & Ryan Lewis",
     "title": "Thrift Shop",
-    "duration": "3 min 49",
-    "style": ["Hip Hop"],
+    "duration": "03:49",
+    "year": "",
+    "style": [
+      "Hip Hop"
+    ],
     "category": "2010s Music",
-    "cover": "img/Macklemore & Ryan Lewis - Thrift Shop.jpg"
+    "cover": "img/Macklemore & Ryan Lewis - Thrift Shop.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Madonna",
     "title": "La Isla Bonita",
-    "duration": "3 min 48",
-    "style": ["Pop"],
+    "duration": "03:48",
+    "year": "",
+    "style": [
+      "Pop"
+    ],
     "category": "80s Music",
-    "cover": "img/Madonna - La Isla Bonita.jpg"
+    "cover": "img/Madonna - La Isla Bonita.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
   {
     "artist": "Madonna",
     "title": "Like A Prayer",
-    "duration": "7 min 04",
-    "style": ["Pop"],
+    "duration": "07:04",
+    "year": "",
+    "style": [
+      "Pop"
+    ],
     "category": "80s Music",
-    "cover": "img/Madonna - Like A Prayer.jpg"
+    "cover": "img/Madonna - Like A Prayer.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Major Lazer",
     "title": "Lean on",
-    "duration": "2 min 56",
-    "style": ["Electronic"],
+    "duration": "02:56",
+    "year": "",
+    "style": [
+      "Electronic"
+    ],
     "category": "2010s Music",
-    "cover": "img/Major Lazer - Lean on.jpg"
+    "cover": "img/Major Lazer - Lean on.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
+  {
+    "artist": "Maroon 5",
+    "title": "Memories",
+    "duration": "03:00",
+    "year": "2019",
+    "style": [
+      "Pop"
+    ],
+    "category": "2010s Music",
+    "cover": "img/default-cover.jpg",
+    "addedAt": "2026-02-02T03:09:00.687Z"
+  },
   {
     "artist": "Marvin Gaye",
     "title": "I heard it trough the grapevine",
     "duration": "",
+    "year": "",
     "style": [],
     "category": "",
-    "cover": "img/Marvin Gaye - I heard it trough the grapevine.jpg"
+    "cover": "img/Marvin Gaye - I heard it trough the grapevine.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
   {
     "artist": "Marvin Gaye",
     "title": "Sexual Healing",
-    "duration": "4 min 08",
-    "style": ["Soul"],
+    "duration": "04:08",
+    "year": "",
+    "style": [
+      "Soul"
+    ],
     "category": "80s Music",
-    "cover": "img/Marvin Gaye - Sexual Healing.jpg"
+    "cover": "img/Marvin Gaye - Sexual Healing.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Massive Attack",
     "title": "Angel",
-    "duration": "6 min 13",
-    "style": ["Trip-Hop"],
+    "duration": "06:13",
+    "year": "",
+    "style": [
+      "Trip-Hop"
+    ],
     "category": "90s Music",
-    "cover": "img/Massive Attack - Angel.jpg"
+    "cover": "img/Massive Attack - Angel.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
   {
     "artist": "Massive Attack",
     "title": "Teardrop",
-    "duration": "5 min 24",
-    "style": ["Trip-Hop"],
+    "duration": "05:24",
+    "year": "",
+    "style": [
+      "Trip-Hop"
+    ],
     "category": "90s Music",
-    "cover": "img/Massive Attack - Teardrop.jpg"
+    "cover": "img/Massive Attack - Teardrop.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Mattafix",
     "title": "Big City Life",
     "duration": "",
+    "year": "",
     "style": [],
     "category": "",
-    "cover": "img/Mattafix - Big City Life.jpg"
+    "cover": "img/Mattafix - Big City Life.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Medley - Sweet Dreams vs Seven Nation Army",
     "title": "Sweet Dreams vs Seven Nation Army",
-    "duration": "3 min 33",
-    "style": ["Mashup"],
+    "duration": "03:33",
+    "year": "",
+    "style": [
+      "Mashup"
+    ],
     "category": "Mixed Years",
-    "cover": "img/Medley - Sweet Dreams vs Seven Nation Army - Sweet Dreams vs Seven Nation Army.jpg"
+    "cover": "img/Medley - Sweet Dreams vs Seven Nation Army - Sweet Dreams vs Seven Nation Army.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Meute",
     "title": "You and Me (Flume Remix)",
-    "duration": "5 min 33",
-    "style": ["Electronic"],
+    "duration": "05:33",
+    "year": "",
+    "style": [
+      "Electronic"
+    ],
     "category": "2010s Music",
-    "cover": "img/Meute - You and Me (Flume Remix).jpg"
+    "cover": "img/Meute - You and Me (Flume Remix).jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "MGMT",
     "title": "Kids",
-    "duration": "4 min 52",
-    "style": ["Indie Pop"],
+    "duration": "04:52",
+    "year": "",
+    "style": [
+      "Indie Pop"
+    ],
     "category": "2000s Music",
-    "cover": "img/MGMT - Kids.jpg"
+    "cover": "img/MGMT - Kids.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Mia",
     "title": "Paper Planes",
     "duration": "",
+    "year": "",
     "style": [],
     "category": "",
-    "cover": "img/Mia - Paper Planes.jpg"
+    "cover": "img/Mia - Paper Planes.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Michael Jackson",
     "title": "Beat it",
-    "duration": "4 min 12",
-    "style": ["Pop"],
+    "duration": "04:12",
+    "year": "",
+    "style": [
+      "Pop"
+    ],
     "category": "80s Music",
-    "cover": "img/Michael Jackson - Beat it.jpg"
+    "cover": "img/Michael Jackson - Beat it.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
   {
     "artist": "Michael Jackson",
     "title": "Billie Jean",
-    "duration": "4 min 53",
-    "style": ["Pop"],
+    "duration": "04:53",
+    "year": "",
+    "style": [
+      "Pop"
+    ],
     "category": "80s Music",
-    "cover": "img/Michael Jackson - Billie Jean.jpg"
+    "cover": "img/Michael Jackson - Billie Jean.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
   {
     "artist": "Michael Jackson",
     "title": "Smooth Criminal",
     "duration": "",
+    "year": "",
     "style": [],
     "category": "",
-    "cover": "img/Michael Jackson - Smooth Criminal.jpg"
+    "cover": "img/Michael Jackson - Smooth Criminal.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
   {
     "artist": "Michael Jackson",
     "title": "They Don't Care About Us",
     "duration": "",
+    "year": "",
     "style": [],
     "category": "",
-    "cover": "img/Michael Jackson - They Don't Care About Us.jpg"
+    "cover": "img/Michael Jackson - They Don't Care About Us.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Michel Polnareff",
     "title": "Lettre a France",
-    "duration": "3 min 42",
-    "style": ["French Pop"],
+    "duration": "03:42",
+    "year": "",
+    "style": [
+      "French Pop"
+    ],
     "category": "70s Music",
-    "cover": "img/Michel Polnareff - Lettre a France.jpg"
+    "cover": "img/Michel Polnareff - Lettre a France.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Michel Sardou",
     "title": "Les lacs du connemara",
-    "duration": "6 min 02",
-    "style": ["French Pop"],
+    "duration": "06:02",
+    "year": "",
+    "style": [
+      "French Pop"
+    ],
     "category": "80s Music",
-    "cover": "img/Michel Sardou - Les lacs du connemara.jpg"
+    "cover": "img/Michel Sardou - Les lacs du connemara.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Midnight Oil",
     "title": "Beds are burning",
     "duration": "",
+    "year": "",
     "style": [],
     "category": "",
-    "cover": "img/Midnight Oil - Beds are burning.jpg"
+    "cover": "img/Midnight Oil - Beds are burning.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Mike Oldfield",
     "title": "Moonlight Shadow",
-    "duration": "5 min 01",
-    "style": ["Pop Rock"],
+    "duration": "05:01",
+    "year": "",
+    "style": [
+      "Pop Rock"
+    ],
     "category": "80s Music",
-    "cover": "img/Mike Oldfield - Moonlight Shadow.jpg"
+    "cover": "img/Mike Oldfield - Moonlight Shadow.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Miki Matsubara",
     "title": "Stay With Me",
-    "duration": "5 min 01",
-    "style": ["City Pop"],
+    "duration": "05:01",
+    "year": "",
+    "style": [
+      "City Pop"
+    ],
     "category": "80s Music",
-    "cover": "img/Miki Matsubara - Stay With Me.jpg"
+    "cover": "img/Miki Matsubara - Stay With Me.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Moby",
     "title": "Porcelain",
     "duration": "",
+    "year": "",
     "style": [],
     "category": "",
-    "cover": "img/Moby - Porcelain.jpg"
+    "cover": "img/Moby - Porcelain.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Modjo",
     "title": "Lady Hear Me Tonight",
-    "duration": "3 min 40",
-    "style": ["House"],
+    "duration": "03:40",
+    "year": "",
+    "style": [
+      "House"
+    ],
     "category": "2000s Music",
-    "cover": "img/Modjo - Lady Hear Me Tonight.jpg"
+    "cover": "img/Modjo - Lady Hear Me Tonight.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Monty Python",
     "title": "Always look on the bright side of life",
-    "duration": "3 min 32",
-    "style": ["Comedy"],
+    "duration": "03:32",
+    "year": "",
+    "style": [
+      "Comedy"
+    ],
     "category": "70s Music",
-    "cover": "img/Monty Python - Always look on the bright side of life.jpg"
+    "cover": "img/Monty Python - Always look on the bright side of life.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Muse",
     "title": "Verona",
     "duration": "",
+    "year": "",
     "style": [],
     "category": "",
-    "cover": "img/Muse - Verona.jpg"
+    "cover": "img/Muse - Verona.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   },
-
   {
     "artist": "Myrskyluodon Maija",
     "title": "Myrskyluodon Maija",
-    "duration": "3 min 11",
-    "style": ["Classical"],
+    "duration": "03:11",
+    "year": "",
+    "style": [
+      "Classical"
+    ],
     "category": "Modern Classical",
-    "cover": "img/Myrskyluodon Maija - Myrskyluodon Maija.jpg"
+    "cover": "img/Myrskyluodon Maija - Myrskyluodon Maija.jpg",
+    "addedAt": "2026-02-02T03:08:57.115Z"
   }
 ];
